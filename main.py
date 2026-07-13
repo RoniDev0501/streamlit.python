4fc5e6fcb7a4164928485fbe29abf7ff63247c07
ronidev0501 <ronivaldodecarvalhobrito@gmail.com>
2026-07-13 11:59:59 -0300

    Refactor chatbot to use Gemini AI model

--- main.py
+++ main.py
@@ -1,52 +1,41 @@
-# titulo
-# input do chat
-# a cada mensagem enviada:
-    # mostrar a mensagem que o usuario enviou no chat
-    # enviar essa mensagem para a IA responder
-    # aparece na tela a resposta da IA
-
-# streamlit - frontend e backend
-
 import streamlit as st
-from openai import OpenAI
+import os
+from dotenv import load_dotenv
+from google import genai
 
-modelo = OpenAI(api_key="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")
+load_dotenv()
 
-st.write("### ChatBot com IA") # markdown
+modelo_ia = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
 
-# session_state = memoria do streamlit
-if not "lista_mensagens" in st.session_state:
-    st.session_state["lista_mensagens"] = []
+st.write('## Roni Chat')  # Markdown
 
-# adicionar uma mensagem
-# st.session_state["lista_mensagens"].append(mensagem)
+if not 'lista_mensagens' in st.session_state:
+    st.session_state['lista_mensagens'] = []
 
-# exibir o histórico de mensagens
-for mensagem in st.session_state["lista_mensagens"]:
-    role = mensagem["role"]
-    content = mensagem["content"]
+texto_usuario = st.chat_input('Digite sua mensagem aqui...')
+
+for mensagem in st.session_state['lista_mensagens']:
+    role = mensagem['role']
+    content = mensagem['content']
     st.chat_message(role).write(content)
 
-mensagem_usuario = st.chat_input("Escreva sua mensagem aqui")
+if texto_usuario:
+    st.chat_message('user').write(texto_usuario)
+    mensagem_usuario = {'role': 'user', 'content': texto_usuario}
+    st.session_state['lista_mensagens'].append(mensagem_usuario)
 
-if mensagem_usuario:
-    # user -> ser humano
-    # assistant -> inteligencia artificial
-    st.chat_message("user").write(mensagem_usuario)
-    mensagem = {"role": "user", "content": mensagem_usuario}
-    st.session_state["lista_mensagens"].append(mensagem)
+    historico_formatado = []
+    for m in st.session_state['lista_mensagens']:
+        role_gemini = 'model' if m['role'] == 'assistant' else 'user'
+        historico_formatado.append({'role': role_gemini, 'parts': [{'text': m['content']}]})
 
-    # resposta da IA
-    resposta_modelo = modelo.chat.completions.create(
-        messages=st.session_state["lista_mensagens"],
-        model="gpt-4o"
-    )
-    
-    resposta_ia = resposta_modelo.choices[0].message.content
+    resposta_ia = modelo_ia.models.generate_content(
+        model='gemini-3.1-flash-lite',
+        contents=historico_formatado,
+    )   
 
-    # exibir a resposta da IA na tela
-    st.chat_message("assistant").write(resposta_ia)
-    mensagem_ia = {"role": "assistant", "content": resposta_ia}
-    st.session_state["lista_mensagens"].append(mensagem_ia)
+    texto_resposta_ia = resposta_ia.text
 
-
+    st.chat_message('assistant').write(texto_resposta_ia)
+    mensagem_ia = {'role': 'assistant', 'content': texto_resposta_ia}
+    st.session_state['lista_mensagens'].append(mensagem_ia)
